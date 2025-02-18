7c6c34eb94fe7c3a6b51bb7041946775ca7e4e3e
helpmeagain <felipe.comarques1208@gmail.com>
2025-02-18 16:03:27 -0300

    feat: update player and enemy sprites

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# dodge-the-creeps
+# Dodge the creeps
  
+Cat sprite: https://ikoiku.itch.io/16-x-16-pixel-art-character-cat
+Enemy sprite: https://chiecola.itch.io/vomfy
+Enemy sprite: https://eduardscarpato.itch.io/toxic-frog-animations-pixel-art-2d-free
--- player.gd
+++ player.gd
@@ -4,11 +4,15 @@ signal hit
 
 @export var speed = 400
 var screen_size
+var is_dead = false  # Adicionando uma variável para controlar o estado de morte
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
 func _process(delta: float) -> void:
+	if is_dead:  # Verifica se o personagem morreu, e não processa mais movimento
+		return
+	
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("mover_direita"):
 		velocity.x += 1
@@ -22,25 +26,27 @@ func _process(delta: float) -> void:
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.animation = "walk"  # Continuar a animação de "walk" enquanto se move
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 	else:
-		$AnimatedSprite2D.stop()
-	
+		$AnimatedSprite2D.animation = "idle"  # Fica na animação "idle" quando não se mover
+
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
-	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
-	
 
 func _on_body_entered(body: Node2D) -> void:
+	if is_dead:
+		return
+	
 	hide()
 	hit.emit()
 
 func start(pos):
 	position = pos
 	show()
+	is_dead = false  # Reseta o estado de morte quando o personagem começa
+	$AnimatedSprite2D.play("idle")  # Começa a animação idle
+
+func die():
+	is_dead = true  # Define o personagem como morto
+	$AnimatedSprite2D.play("death")  # Toca a animação de morte
